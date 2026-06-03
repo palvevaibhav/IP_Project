8330ffb97e0742f24d7dace979db10ebb9858352
vaibhav palve <vaibhav1.palve@ril.com>
2026-06-03 08:38:23 +0530

    done

---
+++ frontend/src/components/Headers.js
@@ -0,0 +1,12 @@
+import react from 'react';
+
+const Headers = () => {
+  return (
+    <div className="headers">
+      <h1>IP Project</h1>
+      <h2>A project to learn about IP</h2>
+    </div>
+  );
+};
+
+export default Headers;
